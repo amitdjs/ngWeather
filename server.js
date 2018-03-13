b73e3329e7ee0db36dae823e6d393d5976a453c8
Amit Kumar Dubey 2 <amidubey@publicisgroupe.net>
2018-03-13 09:51:33 +0530

    PRU: path to index

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(__dirname + "/dist/"));
+app.use(express.static(__dirname));
 
 var port = process.env.PORT || 3000;
 
